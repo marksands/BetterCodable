87d9e087005a39f7cd63d1ec8f5a57573435c29c
Serj Agopian <serjagopian@gmail.com>
2020-02-15 15:31:04 +0100

    Rename Bool tests from saying array to bool

--- Tests/BetterCodableTests/DefaultFalseTests.swift
+++ Tests/BetterCodableTests/DefaultFalseTests.swift
@@ -6,7 +6,7 @@ class DefaultFalseTests: XCTestCase {
         @DefaultFalse var truthy: Bool
     }
     
-    func testDecodingFailableArrayDefaultsToEmptyArray() throws {
+    func testDecodingFailableArrayDefaultsToFalse() throws {
         let jsonData = #"{ "truthy": null }"#.data(using: .utf8)!
         let fixture = try JSONDecoder().decode(Fixture.self, from: jsonData)
         XCTAssertEqual(fixture.truthy, false)
@@ -18,7 +18,7 @@ class DefaultFalseTests: XCTestCase {
         XCTAssertEqual(fixture.truthy, false)
     }
     
-    func testEncodingDecodedFailableArrayDefaultsToEmptyArray() throws {
+    func testEncodingDecodedFailableArrayDefaultsToFalse() throws {
         let jsonData = #"{ "truthy": null }"#.data(using: .utf8)!
         var _fixture = try JSONDecoder().decode(Fixture.self, from: jsonData)
         
@@ -29,7 +29,7 @@ class DefaultFalseTests: XCTestCase {
         XCTAssertEqual(fixture.truthy, true)
     }
     
-    func testEncodingDecodedFulfillableArrayRetainsContents() throws {
+    func testEncodingDecodedFulfillableBoolRetainsValue() throws {
         let jsonData = #"{ "truthy": true }"#.data(using: .utf8)!
         let _fixture = try JSONDecoder().decode(Fixture.self, from: jsonData)
         let fixtureData = try JSONEncoder().encode(_fixture)
